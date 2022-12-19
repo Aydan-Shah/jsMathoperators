5762ac00c35fff7e8ee71e551d9175ef66946958
Aydan-Shah <aydanhilal5@gmail.com>
2022-12-19 22:57:42 +0400

    o

---
+++ app.js
@@ -0,0 +1,32 @@
+let value;
+const value1 = 10;
+const value2 = 4;
+
+/* Arifmetik Operatorlar */
+// value = value1 + value2;
+// value = value1 - value2;
+// value = value1 * value2;
+// value = value1 / value2;
+// value = value1 % value2;
+
+/* Math */
+value = Math.PI;
+value = Math.E;
+value = Math.round(3.6);//4
+value = Math.ceil(3.2); //3
+value = Math.floor(3.7) //3
+value = Math.sqrt(16); //4
+value = Math.abs(-10); //10
+value = Math.pow(8,3)// 512
+value = Math.max(10, -1, 100, 32); // 100
+value = Math.min(10, -1, 100, 32); // -1
+value = Math.random(); // 0 1 arasi
+value = Math.floor(Math.random()*20 + 1) //1 ve 21 arasi 
+
+
+
+
+
+
+
+console.log(value);
